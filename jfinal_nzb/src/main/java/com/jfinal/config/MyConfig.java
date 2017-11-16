059956d48bb8b6c73adf152c2f37aa6f51a5b870
niezhongbing <1686763368@qq.com>
2017-11-16 17:34:21 +0800

    jfinal初步整合完

--- jfinal_nzb/src/main/java/com/jfinal/config/MyConfig.java
+++ jfinal_nzb/src/main/java/com/jfinal/config/MyConfig.java
@@ -1,20 +1,27 @@
 package com.jfinal.config;
 
+import com.jfinal.Engine.Method;
+import com.jfinal.Engine.SharedObject;
+import com.jfinal.Engine.StaticMethodS;
 import com.jfinal.Rotue.AdminRoute;
 import com.jfinal.Rotue.FrontRoute;
 import com.jfinal.controller.InjectController;
+import com.jfinal.controller.LoginController;
 import com.jfinal.core.JFinal;
 import com.jfinal.ext.handler.ContextPathHandler;
 import com.jfinal.interceptor.GlobalInterceptor;
-import com.jfinal.interceptor.InjectInterceptor;
+import com.jfinal.kit.PathKit;
 import com.jfinal.kit.PropKit;
 import com.jfinal.model._MappingKit;
 import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.cron4j.Cron4jPlugin;
 import com.jfinal.plugin.druid.DruidPlugin;
+import com.jfinal.plugin.ehcache.EhCachePlugin;
 import com.jfinal.template.Engine;
 
 public class MyConfig extends JFinalConfig{
 
+	//读取配置文件
 	@Override
 	public void configConstant(Constants me) {
 		// TODO Auto-generated method stub
@@ -22,21 +29,33 @@ public class MyConfig extends JFinalConfig{
 		PropKit.use("a_little_config.txt");
 	}
 
+	//路由配置路径
 	@Override
 	public void configRoute(Routes me) {
 		// TODO Auto-generated method stub
-		me.add("/inject", InjectController.class);
+		me.add("/login", LoginController.class);
+		/*me.add("/inject", InjectController.class);
 		me.add(new FrontRoute());
-		me.add(new AdminRoute());
+		me.add(new AdminRoute());*/
 	}
 	public static DruidPlugin createDruidPlugin() {
 		return new DruidPlugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password").trim());
 	}
+	
+	//定义公共模板
 	@Override
 	public void configEngine(Engine me) {
 		// TODO Auto-generated method stub
+		me.setBaseTemplatePath(PathKit.getWebRootPath());
+		me.addSharedFunction("/front/common.html");
+		
+		//将某个类的方法共享出来
+		me.addSharedStaticMethod(StaticMethodS.class);
+		me.addSharedMethod(Method.class);
+		me.addSharedObject("sm", new SharedObject());
 	}
 
+	//配置插件
 	@Override
 	public void configPlugin(Plugins me) {
 		// 配置C3p0数据库连接池插件
@@ -46,11 +65,26 @@ public class MyConfig extends JFinalConfig{
 		
 		// 配置ActiveRecord插件
 		ActiveRecordPlugin arp = new ActiveRecordPlugin(druidPlugin);
+		
+		//通过SQL文件读取SQL
+		arp.setBaseSqlTemplatePath(PathKit.getWebRootPath()+"/WEB-INF");
+		arp.addSqlTemplate("sql/jfinal.sql");
+		
 		// 所有映射在 MappingKit 中自动化搞定
 		_MappingKit.mapping(arp);
 		me.add(arp);
+		
+		
+		//缓存
+		me.add(new EhCachePlugin());
+		
+		
+		//任务调度
+		Cron4jPlugin cron = new Cron4jPlugin(PropKit.use("cron.txt"));
+		me.add(cron);
 	}
 
+	//配置拦截器
 	@Override
 	public void configInterceptor(Interceptors me) {
 		// TODO Auto-generated method stub
@@ -58,11 +92,14 @@ public class MyConfig extends JFinalConfig{
 		//me.addGlobalServiceInterceptor(new InjectInterceptor());
 	}
 
+	//接管所有 web 请求，并对应用拥有完全的控制权
 	@Override
 	public void configHandler(Handlers me) {
 		// TODO Auto-generated method stub
 		me.add(new ContextPathHandler("ctx"));
 	}
+	
+	
 	public static void main(String[] args) {
 		JFinal.start("src/main/webapp", 8091, "/", 5);
 
--- jfinal_nzb/src/main/java/com/jfinal/controller/IndexController.java
+++ jfinal_nzb/src/main/java/com/jfinal/controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.jfinal.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.BeforeClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.jfinal.aop.Before;
 import com.jfinal.aop.Clear;
@@ -11,14 +14,21 @@ import com.jfinal.interceptor.Ainterceptor;
 import com.jfinal.interceptor.BInterceptor;
 import com.jfinal.interceptor.ClassControllerInterceptort;
 import com.jfinal.interceptor.MethodIntercept;
+import com.jfinal.kit.Kv;
 import com.jfinal.model.Blog;
 import com.jfinal.plugin.activerecord.Db;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Record;
 import com.jfinal.plugin.activerecord.SqlPara;
+import com.jfinal.plugin.activerecord.tx.Tx;
+import com.jfinal.plugin.ehcache.CacheInterceptor;
+import com.jfinal.plugin.ehcache.CacheName;
+import com.jfinal.plugin.ehcache.EvictInterceptor;
 @Before(ClassControllerInterceptort.class)
 public class IndexController extends Controller{
+	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
 
+	
 	@Before(MethodIntercept.class)
 	public void index(){
 		String id = getPara("msg","default");
@@ -50,11 +60,66 @@ public class IndexController extends Controller{
 		render("index.html");
 	}
 	
+	@Before(CacheInterceptor.class)
+	@CacheName("testEhcache")
 	public void query(){
-		 Page<Blog> paginate = Blog.dao.paginate(1, 2,  "select *","from blog");
-		renderJson(paginate);
-	}
-	public void update(){
+		String sql = "select * from blog";
+		List<Object> query = Db.query(sql);
+		//分页
+		// Page<Blog> paginate = Blog.dao.paginate(1, 2,"select *","from blog group by title");
 		
+		 //如果为true就取去除重复之后，false就取相同的
+		//Page<Blog> paginate = Blog.dao.paginate(1, 2,true,"select *","from blog group by title");
+		
+		 //第一条sql查数据的总数，第二条查询数据。查询条件要一样，查询的条数以第一条sql为主
+		// Page<Blog> paginate = Blog.dao.paginateByFullSql(1, 7,"select count(*) from blog where id != 1","select * from blog where id != 1");
+		 
+		renderJson(query);
+	}
+	
+	@Before(EvictInterceptor.class)
+	@CacheName("testEhcache")
+	public void updateEhcache(){
+		getModel(Blog.class).update();
+		renderJson("更新缓存成功!!!!");
+		renderJson("更新缓存失败!!!");
+		
+	}
+	
+	//声明式事务，只要在这个里面有报错通通回滚所修改的
+	@Before(Tx.class)
+	public void update() throws Exception{
+		Record record = new Record();
+		Blog b1 = new Blog();
+		b1.setContent("1");
+		b1.setId(1);
+		b1.setTitle("222");
+		
+		Blog b2 = new Blog();
+		b2.setContent("2");
+		b2.setId(null);
+		b2.setTitle("2");
+		
+		List<Blog> list = new ArrayList<Blog>();
+		list.add(b1);
+		list.add(b2);
+		Blog blog = getModel(Blog.class);
+		int[] batchUpdate = Db.batchUpdate(list, list.size());
+		//throw new Exception();
+		for (int i = 0; i < batchUpdate.length; i++) {
+			logger.info("batchUpdate {}",batchUpdate[i]);
+		}
+		render("index.html");
+	}
+	
+	//动态生成sql
+	public void sqlManager(){
+		SqlPara sql = Db.getSqlPara("test1",1);
+		List<Record> find = Db.find(sql);
+		
+		Kv cond = Kv.by("title", "3");
+		SqlPara sqlPara = Db.getSqlPara("test2", cond);
+		List<Record> find2 = Db.find(sqlPara);
+		renderJson(find2);
 	}
 }
